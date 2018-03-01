d123ce59b308c0bc2ba8b288b614b7a678f1bf55
Alena Tarasenko <tarasenkoalenka@gmail.com>
2018-03-01 20:09:34 +0200

    my first commit

---
+++ src/test/java/allureExample/TrelloCards.java
@@ -0,0 +1,15 @@
+package allureExample;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import org.testng.annotations.Test;
+
+@Epic("Trelo Api")
+@Feature("Cards Action")
+public class TrelloCards {
+    @Test
+
+    public void createCard(){
+
+    }
+}
